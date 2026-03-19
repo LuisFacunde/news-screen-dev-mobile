44753a9441634da89eb2b70f41df3edc0817f077
Luis Facunde <luisfacunde19@gmail.com>
2026-03-19 17:42:18 -0300

    Perf: remoção de arquivos e dados não utilizados.

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,5 @@
 import { Header } from "@/components/Header";
 import { NewsCard } from "@/components/NewsCard";
-import { SectionListExample } from "@/components/SectionListExample";
 import { NEWS_ITEM } from "@/data/newsItem";
 import { useState } from "react";
 import { FlatList, StyleSheet, Text, View } from "react-native";
@@ -8,21 +7,6 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
     const [isDark, setIsDark] = useState(false);
-    const [isEnabled, setIsEnabled] = useState(false);
-
-    let MyComponent;
-    if (isEnabled) {
-        MyComponent = <SectionListExample />;
-    } else {
-        MyComponent = (
-            <FlatList
-                data={NEWS_ITEM}
-                keyExtractor={(item) => item.id.toString()}
-                renderItem={({ item }) => <NewsCard isDark={isDark} news={item} />}
-                contentContainerStyle={{ paddingBottom: 16 }}
-            />
-        );
-    }
 
     return (
         <SafeAreaView
@@ -30,7 +14,14 @@ export default function Index() {
         >
             <Header isDark={isDark} onToggleTheme={setIsDark} />
 
-            <View style={{ flex: 1 }}>{MyComponent}</View>
+            <View style={{ flex: 1 }}>
+                <FlatList
+                    data={NEWS_ITEM}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({ item }) => <NewsCard isDark={isDark} news={item} />}
+                    contentContainerStyle={{ paddingBottom: 16 }}
+                />
+            </View>
         </SafeAreaView>
     );
 }
