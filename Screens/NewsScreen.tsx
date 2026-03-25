fc3994120a8e2f1a9f799ac7ba35ec0ec6994cff
Luis Facunde <luisfacunde19@gmail.com>
2026-03-25 11:37:10 -0300

    feat(screen): criação da tela de perfil com foto, input e butão de salvar com alert.

--- Screens/NewsScreen.tsx
+++ Screens/NewsScreen.tsx
@@ -7,50 +7,67 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { useRouter } from "expo-router";
 
 export default function NewsScreen() {
-    const router = useRouter();
-    const [isDark, setIsDark] = useState(false);
+   const router = useRouter();
+   const [isDark, setIsDark] = useState(false);
 
-    return (
-        <SafeAreaView
-            style={{ flex: 1, backgroundColor: isDark ? "#020817" : "#f0f9ff" }}
-        >
-            {/* Header */}
-            <Header isDark={isDark} onToggleTheme={setIsDark} />
+   return (
+      <SafeAreaView
+         style={{ flex: 1, backgroundColor: isDark ? "#020817" : "#f0f9ff" }}
+      >
+         {/* Header */}
+         <Header isDark={isDark} onToggleTheme={setIsDark} />
 
-            {/* Notícias */}
-            <View style={{ flex: 1 }}>
-                <FlatList
-                    data={NEWS_ITEM}
-                    keyExtractor={(item) => item.id.toString()}
-                    renderItem={({ item }) => <NewsCard isDark={isDark} news={item} />}
-                    contentContainerStyle={{ paddingBottom: 16 }}
-                />
+         {/* Notícias */}
+         <View style={{ flex: 1 }}>
+            <FlatList
+               data={NEWS_ITEM}
+               keyExtractor={(item) => item.id.toString()}
+               renderItem={({ item }) => (
+                  <NewsCard isDark={isDark} news={item} />
+               )}
+               contentContainerStyle={{ paddingBottom: 16 }}
+            />
+         </View>
+
+         {/* Footer */}
+         <View
+            style={[
+               styles.footer,
+               {
+                  borderTopColor: isDark ? "#1e293b" : "#E2E8F0",
+                  backgroundColor: isDark ? "#0f172a" : "#ffffff",
+               },
+            ]}
+         >
+            <Text
+               style={[
+                  styles.footerText,
+                  { color: isDark ? "#80ed99" : "#008000" },
+               ]}
+            >
+               Atualização concluída!
+            </Text>
+            <View style={styles.footerButton}>
+               <Button title="Voltar" onPress={() => router.back()} />
             </View>
-
-            {/* Footer */}
-            <View style={[styles.footer, { borderTopColor: isDark ? "#1e293b" : "#E2E8F0", backgroundColor: isDark ? "#0f172a" : "#ffffff" }]} >
-                <Text style={[styles.footerText, { color: isDark ? "#80ed99" : "#008000" }]}>Atualização concluída!</Text> 
-                <View style={styles.footerButton}>
-                    <Button title="Voltar" onPress={() => router.back()} />
-                </View>
-            </View>
-        </SafeAreaView>
-    );
+         </View>
+      </SafeAreaView>
+   );
 }
 
 const styles = StyleSheet.create({
-    footer: {
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 16,
-        borderTopWidth: 1,
-    },
-    footerText: {
-        fontSize: 16,
-        fontWeight: "600",
-    },
-    footerButton: {
-        marginTop: 16,
-        width: "110%",
-    },
+   footer: {
+      justifyContent: "center",
+      alignItems: "center",
+      padding: 16,
+      borderTopWidth: 1,
+   },
+   footerText: {
+      fontSize: 16,
+      fontWeight: "600",
+   },
+   footerButton: {
+      marginTop: 16,
+      width: "100%",
+   },
 });
--- Screens/ProfileScreen.tsx
+++ Screens/ProfileScreen.tsx
@@ -1,64 +1,168 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import {
+   Button,
+   StyleSheet,
+   Text,
+   TextInput,
+   View,
+   Image,
+   Pressable,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useRouter } from "expo-router";
 import { useState } from "react";
 import { ThemeSwitch } from "@/components/ThemeSwitch";
 
 export default function ProfileScreen() {
-    const router = useRouter();
-    const [isDark, setIsDark] = useState(false);
-    const backgroundColorHeader = isDark ? "#0f172a" : "#ffffff";
-    const borderColorHeader = isDark ? "#1e293b" : "#E2E8F0";
+   const router = useRouter();
+   const [isDark, setIsDark] = useState(false);
+   const backgroundColorHeader = isDark ? "#0f172a" : "#ffffff";
+   const borderColorHeader = isDark ? "#1e293b" : "#E2E8F0";
 
-    return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: isDark ? "#020817" : "#f0f9ff" }}>
-            
-            {/* Header */}
-            <View style={[styles.header, { backgroundColor: backgroundColorHeader, borderBottomColor: borderColorHeader }]}>
-                <Text style={[styles.title, { color: isDark ? "#fff" : "#000" }]}>Tela de Perfil</Text>
-                <ThemeSwitch isDark={isDark} onToggle={setIsDark}/>
-            </View>
+   return (
+      <SafeAreaView
+         style={{ flex: 1, backgroundColor: isDark ? "#020817" : "#f0f9ff" }}
+      >
+         {/* Header */}
+         <View
+            style={[
+               styles.header,
+               {
+                  backgroundColor: backgroundColorHeader,
+                  borderBottomColor: borderColorHeader,
+               },
+            ]}
+         >
+            <Text style={[styles.title, { color: isDark ? "#fff" : "#000" }]}>
+               Tela de Perfil
+            </Text>
+            <ThemeSwitch isDark={isDark} onToggle={setIsDark} />
+         </View>
 
-            <View>
-                <Text> CONTEÚDO </Text>
-            </View>
+         {/* Foto e Input */}
+         <View
+            style={[
+               styles.content,
+               { backgroundColor: isDark ? "#020817" : "#f0f9ff" },
+            ]}
+         >
+            <Image
+               source={require("@/assets/images/foto-perfil-gemini.png")}
+               style={styles.image}
+            />
+            <Text
+               style={[styles.label, { color: isDark ? "#fff" : "#1e293b" }]}
+            >
+               Nome Completo
+            </Text>
+            <TextInput
+               style={[styles.input, { color: isDark ? "#f1f5f9" : "#0f172a" }]}
+               placeholder="Digite seu nome"
+               placeholderTextColor={isDark ? "#475569" : "#94a3b8"}
+            />
+            <Pressable
+               style={[
+                  styles.buttonContent,
+                  { backgroundColor: isDark ? "#38b000" : "#008000" },
+               ]}
+               onPress={() => alert("Perfil salvo!")}
+            >
+               <Text style={styles.buttonText}>Salvar Perfil</Text>
+            </Pressable>
+         </View>
 
-            {/* Footer */}
-            <View style={[styles.footer, { borderTopColor: isDark ? "#1e293b" : "#E2E8F0", backgroundColor: isDark ? "#0f172a" : "#ffffff" }]}>
-                <Text style={[styles.footerText, { color: isDark ? "#80ed99" : "#008000" }]}>Atualização concluída!</Text>
-                <View style={styles.footerButton}>
-                    <Button title="Voltar" onPress={() => router.back()} />
-                </View>
+         {/* Footer */}
+         <View
+            style={[
+               styles.footer,
+               {
+                  borderTopColor: isDark ? "#1e293b" : "#E2E8F0",
+                  backgroundColor: isDark ? "#0f172a" : "#ffffff",
+               },
+            ]}
+         >
+            <Text
+               style={[
+                  styles.footerText,
+                  { color: isDark ? "#38b000" : "#008000" },
+               ]}
+            >
+               Atualização concluída!
+            </Text>
+            <View style={styles.footerButton}>
+               <Button title="Voltar" onPress={() => router.back()} />
             </View>
-        </SafeAreaView>
-    );
+         </View>
+      </SafeAreaView>
+   );
 }
 
 const styles = StyleSheet.create({
-    header: {
-        flexDirection: "row",
-        alignItems: "center",
-        paddingHorizontal: 16,
-        paddingVertical: 14,
-        borderBottomWidth: 1,
-    },
-    title: {
-        flex: 1,
-        fontSize: 15,
-        fontWeight: "600",
-    },
-    footer: {
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 16,
-        borderTopWidth: 1,
-    },
-    footerText: {
-        fontSize: 16,
-        fontWeight: "600",
-    },
-    footerButton: {
-        marginTop: 16,
-        width: "110%",
-    },
+   header: {
+      flexDirection: "row",
+      alignItems: "center",
+      paddingHorizontal: 16,
+      paddingVertical: 14,
+      borderBottomWidth: 1,
+   },
+   title: {
+      flex: 1,
+      fontSize: 15,
+      fontWeight: "600",
+   },
+   footer: {
+      justifyContent: "center",
+      alignItems: "center",
+      padding: 16,
+   },
+   footerText: {
+      fontSize: 16,
+      fontWeight: "600",
+   },
+   footerButton: {
+      marginTop: 16,
+      width: "100%",
+   },
+   content: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      paddingHorizontal: 24,
+      gap: 0,
+   },
+   label: {
+      fontSize: 14,
+      fontWeight: "600",
+      alignSelf: "flex-start",
+      marginBottom: 6,
+   },
+   input: {
+      height: 54,
+      borderColor: "gray",
+      borderWidth: 1,
+      borderRadius: 12,
+      paddingHorizontal: 16,
+      alignSelf: "stretch",
+      marginBottom: 32,
+   },
+   image: {
+      width: 150,
+      height: 150,
+      resizeMode: "cover",
+      borderRadius: 75,
+      borderWidth: 3,
+      borderColor: "#669bbc",
+      marginBottom: 56,
+   },
+   buttonContent: {
+      paddingVertical: 10,
+      borderRadius: 14,
+      alignSelf: "stretch",
+   },
+   buttonText: {
+      color: "#fff",
+      fontSize: 18,
+      fontWeight: "600",
+      textAlign: "center",
+      padding: 5,
+   },
 });
